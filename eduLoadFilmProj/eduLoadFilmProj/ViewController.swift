5d71dbe7bd6911928e3b5cdd3daf8bc1629be689
ilia <kravets.ilia@gmail.com>
2023-12-17 19:53:46 +0400

    Project started

---
+++ eduLoadFilmProj/eduLoadFilmProj/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  eduLoadFilmProj
+//
+//  Created by Joseph on 17/12/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
