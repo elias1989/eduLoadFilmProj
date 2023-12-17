d814d0caea67e5fa94f3f92b0adb78c2cb5284e7
ilia <kravets.ilia@gmail.com>
2023-12-17 21:15:44 +0400

    ViewController and FilmVewCOntroller added. Button and  presenation methods added

--- eduLoadFilmProj/eduLoadFilmProj/ViewController.swift
+++ eduLoadFilmProj/eduLoadFilmProj/ViewController.swift
@@ -11,9 +11,24 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let myButton = UIButton(type: .system)
+        myButton.setTitle("Load Films", for: .normal)
+        myButton.frame = CGRect(x: 100, y: 350, width: 200, height: 40)
+        myButton.addTarget(self, action: #selector(buttonTapped), for: .touchUpInside)
+        view.addSubview(myButton)
+        
     }
 
+    @objc func buttonTapped() {
+        let filmViewController = FilmViewController()
+        
+        let navigationController = UINavigationController(rootViewController: filmViewController)
+
+        navigationController.modalPresentationStyle = .fullScreen
+
+        present(navigationController, animated: true, completion: nil)
+    }
 
 }
 
