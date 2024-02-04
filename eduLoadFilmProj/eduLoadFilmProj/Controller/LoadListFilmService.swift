95ff6cfb6ae71fd8ccc539884b223209e25600f2
ilia <kravets.ilia@gmail.com>
2024-02-04 22:22:38 +0400

    Films are loading page by page. center spinner configured. Bottom spinner found nil.stopped here

--- eduLoadFilmProj/eduLoadFilmProj/Controller/LoadListFilmService.swift
+++ eduLoadFilmProj/eduLoadFilmProj/Controller/LoadListFilmService.swift
@@ -6,19 +6,21 @@ class LoadListFilmService {
     
     func fetchMovies(page: Int, completion: @escaping ([Movie]?) -> Void) {
         let urlString = "https://api.themoviedb.org/3/movie/popular?api_key=\(apiKey)&page=\(page)"
-            
-            guard let url = URL(string: urlString) else {
+           
+        print(page)
+        
+        guard let url = URL(string: urlString) else {
                 completion(nil)
                 return
             }
             
-            URLSession.shared.dataTask(with: url) { data, _, error in
+        URLSession.shared.dataTask(with: url) { data, _, error in
                 guard let data = data, error == nil else {
                     completion(nil)
                     return
             }
             
-            do {
+        do  {
                 let decoder = JSONDecoder()
                 let result = try decoder.decode(MoviesResponse.self, from: data)
                 let movies = result.results
--- eduLoadFilmProj/eduLoadFilmProj/View/FilmViewController.swift
+++ eduLoadFilmProj/eduLoadFilmProj/View/FilmViewController.swift
@@ -5,34 +5,30 @@ class FilmViewController: UIViewController {
     var tableView: UITableView!
     
     var movies: [Movie] = []
-
+    
     let movieService = LoadListFilmService()
     
     var isLoadingNextPage: Bool = false
     var currentPage: Int = 1
+    
+    var centerSpinner: UIActivityIndicatorView!
+    var isLoadingFirstTime: Bool = true
+    
+    
+    var bottomSpinner: UIActivityIndicatorView!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         setupTableView()
         loadMovies()
-       
+        isLoadingFirstTime = false
+        setupSpinners()
+        startCenterSpinner()
+        
     }
     
-    private func loadMovies() {
-        
-        movieService.fetchMovies(page: currentPage) {  movies in
-            
-            if let movies = movies {
-                self.movies = movies
-                DispatchQueue.main.async {
-                    self.tableView.reloadData()
-                }
-            } else {
-                print("Fail")
-            }
-        }
-    }
     
     private func setupTableView() {
         tableView = UITableView(frame: view.bounds, style: .plain)
@@ -42,10 +38,84 @@ class FilmViewController: UIViewController {
         view.addSubview(tableView)
     
     }
+    
+    
+    private func loadMovies() {
+        
+    self.startBottomSpinner()
+  DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+      
+      
+      
+      self.movieService.fetchMovies(page: self.currentPage) { newMovies in
+          if let movies = newMovies {
+              self.movies += movies
+              DispatchQueue.main.async {
+                  self.tableView.reloadData()
+              }
+          } else {
+              print("Upload failed. No more pages")
+          }
+      }
+           
+      self.stopBottomSpinner()
+      guard self.isLoadingFirstTime == true else { return self.stopCenterSpinner() }
+            
+    }
+ }
+        
+    
+    private func setupSpinners() {
+        centerSpinner = UIActivityIndicatorView(style: .large)
+        centerSpinner.center = view.center
+        view.addSubview(centerSpinner)
+        
+        bottomSpinner = UIActivityIndicatorView(style: .large)
+        bottomSpinner.center = CGPoint(x: view.center.x, y: view.bounds.height - 10)
+        view.addSubview(bottomSpinner)
+    }
+    
+    
+    private func startCenterSpinner() {
+            centerSpinner.startAnimating()
+        }
+    
+    private func stopCenterSpinner() {
+            centerSpinner.stopAnimating()
+        }
+    
+    
+    //to be continues. bottom spinner found nil.
+    private func startBottomSpinner() {
+            bottomSpinner.startAnimating()
+        }
+
+        private func stopBottomSpinner() {
+            bottomSpinner.stopAnimating()
+        }
+    
+    
+    func loadNextPage() {
+        guard !isLoadingNextPage else {
+                  return
+              }
+
+        isLoadingNextPage = true
+        currentPage += 1
+        
+        
+       // startBottomSpinner()
+        loadMovies()
+        //stopBottomSpinner()
+        
+        isLoadingNextPage = false
+    }
+    
 }
 
 
 extension FilmViewController: UITableViewDelegate, UITableViewDataSource {
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return movies.count
     }
@@ -56,48 +126,21 @@ extension FilmViewController: UITableViewDelegate, UITableViewDataSource {
         cell.configure(with: movie)
         
         if indexPath.row == movies.count - 1 && !isLoadingNextPage {
-            loadNextPage()
+            print(indexPath.row)
+            
+            //tableView.scrollToRow(at: indexPath.row - 1, at: .top, animated: true)
         }
         
         return cell
     }
     
     func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
-            // Check if the last cell is displayed
+            //Check if the last cell is displayed
             if indexPath.row == movies.count - 1 && !isLoadingNextPage {
+               print("this is the last page")
                 loadNextPage()
             }
         }
     
 }
 
-
-
-extension FilmViewController: UIScrollViewDelegate {
-    
-//    func scrollViewDidScroll(_ scrollView: UIScrollView) {
-//        let contentOffsetY = scrollView.contentOffset.y
-//        let contentHeight = scrollView.contentSize.height
-//        let scrollViewHeight = scrollView.frame.size.height
-//
-//        let isAtBottom = contentOffsetY + scrollViewHeight >= contentHeight
-//
-//        if isAtBottom && !isLoadingNextPage {
-//            loadNextPage()
-//        }
-//
-//    }
-    
-    func loadNextPage() {
-        guard !isLoadingNextPage else {
-                  return
-              }
-
-              isLoadingNextPage = true
-              currentPage += 1
-
-              loadMovies()
-              isLoadingNextPage = false
-    }
-    
-}
