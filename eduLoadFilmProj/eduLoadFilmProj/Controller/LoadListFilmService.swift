3fac4531818461f687c907e270b1ee9f5713114f
ilia <kravets.ilia@gmail.com>
2024-02-22 21:36:45 +0400

    Task 4.3 done. Last issue is to rename Project as recommended

--- eduLoadFilmProj/eduLoadFilmProj/Controller/LoadListFilmService.swift
+++ eduLoadFilmProj/eduLoadFilmProj/Controller/LoadListFilmService.swift
@@ -1,36 +1,61 @@
 import Foundation
+import UIKit
 
-class LoadListFilmService {
-
+class LoadListFilmService: UIViewController {
+    
     private let apiKey = "feacf88cd81377f6cfa24e512f1c61de"
     
-    func fetchMovies(page: Int, completion: @escaping ([Movie]?) -> Void) {
+    
+    func fetchMovies(page: Int, completion: @escaping ([Movie]?, Error?) -> Void) {
+        
         let urlString = "https://api.themoviedb.org/3/movie/popular?api_key=\(apiKey)&page=\(page)"
-           
-        print(page)
         
         guard let url = URL(string: urlString) else {
-                completion(nil)
+            let urlError = NSError(domain: "URL", code: -1, userInfo: [NSLocalizedDescriptionKey: "Invalid URL"])
+            completion(nil, urlError)
+            return
+        }
+        
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            guard let data = data, let httpResponse = response as? HTTPURLResponse, error == nil else {
+                let networkError = NSError(domain: "Network", code: -1, userInfo: [NSLocalizedDescriptionKey: "Error in network request"])
+                completion(nil, networkError)
                 return
             }
             
-        URLSession.shared.dataTask(with: url) { data, _, error in
-                guard let data = data, error == nil else {
-                    completion(nil)
-                    return
-            }
             
-        do  {
-                let decoder = JSONDecoder()
-                let result = try decoder.decode(MoviesResponse.self, from: data)
-                let movies = result.results
-                completion(movies)
-            } catch {
-                print("Error decoding JSON: \(error)")
-                completion(nil)
+            if 200 ..< 300 ~= httpResponse.statusCode {
+                do  {
+                    let decoder = JSONDecoder()
+                    let result = try decoder.decode(MoviesResponse.self, from: data)
+                    let movies = result.results
+                    completion(movies, nil)
+                } catch {
+                    print("Error decoding JSON: \(error)")
+                    let decodingError = NSError(domain: "Decoding", code: httpResponse.statusCode, userInfo: [NSLocalizedDescriptionKey: "Error decoding JSON: \(error)"])
+                    completion(nil, decodingError)
+                }
+                
+            } else {
+                print("HTTP Response Error: \(httpResponse.statusCode)")
+                let httpResponseError = NSError(domain: "HTTP", code: httpResponse.statusCode, userInfo: [NSLocalizedDescriptionKey: "HTTP Response Error"])
+                completion(nil, httpResponseError)
             }
             
         }.resume()
+        
+        
+        func showAlert(with title: String, message: String) {
+            let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+            let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+            alert.addAction(okAction)
+            
+            DispatchQueue.main.async {
+                self.present(alert, animated: true, completion: nil)
+            }
+        }
+        
     }
+    
     
 }
--- eduLoadFilmProj/eduLoadFilmProj/View/FilmViewController.swift
+++ eduLoadFilmProj/eduLoadFilmProj/View/FilmViewController.swift
@@ -14,9 +14,7 @@ class FilmViewController: UIViewController {
     var centerSpinner: UIActivityIndicatorView!
     var isLoadingFirstTime: Bool = true
     
-    
     var bottomSpinner: UIActivityIndicatorView!
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -29,40 +27,39 @@ class FilmViewController: UIViewController {
         
     }
     
-    
     private func setupTableView() {
         tableView = UITableView(frame: view.bounds, style: .plain)
         tableView.delegate = self
         tableView.dataSource = self
         tableView.register(TableViewFilmCell.self, forCellReuseIdentifier: TableViewFilmCell.reuseIdentifier)
         view.addSubview(tableView)
-    
     }
     
     
     private func loadMovies() {
         
-    //startBottomSpinner()
-  DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
-      
-      self.movieService.fetchMovies(page: self.currentPage) { newMovies in
-          if let movies = newMovies {
-              self.movies += movies
-              DispatchQueue.main.async {
-                  self.tableView.reloadData()
-              }
-          } else {
-              print("Upload failed. No more pages")
-              //make alert here
-          }
-      }
-           
-      //self.stopBottomSpinner()
-      guard self.isLoadingFirstTime == true else { return self.stopCenterSpinner() }
+        DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
             
+            self.movieService.fetchMovies(page: self.currentPage) { (newMovies, error) in
+                if let movies = newMovies {
+                    
+                    self.movies += movies
+                    DispatchQueue.main.async {
+                        self.tableView.reloadData()
+                        
+                    }
+                } else if let error = error {
+                    
+                    self.showAlert(title: "Error", message: error.localizedDescription)
+                    
+                }
+            }
+            
+            guard self.isLoadingFirstTime == true else { return self.stopCenterSpinner() }
+            
+        }
     }
- }
-        
+    
     
     private func setupSpinners() {
         centerSpinner = UIActivityIndicatorView(style: .large)
@@ -76,21 +73,31 @@ class FilmViewController: UIViewController {
     
     
     private func startCenterSpinner() {
-            centerSpinner.startAnimating()
-        }
+        centerSpinner.startAnimating()
+    }
     
     private func stopCenterSpinner() {
-            centerSpinner.stopAnimating()
-        }
+        centerSpinner.stopAnimating()
+    }
     
     private func startBottomSpinner() {
-            bottomSpinner.startAnimating()
-        }
-
-        private func stopBottomSpinner() {
-            bottomSpinner.stopAnimating()
-        }
+        bottomSpinner.startAnimating()
+    }
     
+    private func stopBottomSpinner() {
+        bottomSpinner.stopAnimating()
+    }
+    
+    
+    private func showAlert(title: String, message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+        alert.addAction(okAction)
+        
+        DispatchQueue.main.async {
+            self.present(alert, animated: true, completion: nil)
+        }
+    }
     
     func loadNextPage() {
         guard !isLoadingNextPage else {
@@ -102,10 +109,9 @@ class FilmViewController: UIViewController {
         
         startBottomSpinner()
         
-        // Fetch movies on a background queue
         DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
             
-            self.movieService.fetchMovies(page: self.currentPage) { newMovies in
+            self.movieService.fetchMovies(page: self.currentPage) { (newMovies, error) in
                 if let movies = newMovies {
                     self.movies += movies
                     DispatchQueue.main.async {
@@ -113,18 +119,18 @@ class FilmViewController: UIViewController {
                         self.stopBottomSpinner()
                         self.isLoadingNextPage = false
                     }
-                } else {
-                    print("Failed to fetch more movies. No more pages.")
-                    // Handle error or show an alert
+                } else if let error = error {
+                    
+                    self.showAlert(title: "Error", message: error.localizedDescription)
                     DispatchQueue.main.async {
                         self.stopBottomSpinner()
-                        self.isLoadingNextPage = false
                     }
+                    
                 }
+                
             }
             
         }
-        
         
     }
     
@@ -136,81 +142,21 @@ extension FilmViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return movies.count
     }
-
+    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: TableViewFilmCell.reuseIdentifier, for: indexPath) as! TableViewFilmCell
         let movie = movies[indexPath.row]
         cell.configure(with: movie)
         
-        if indexPath.row == movies.count - 1 && !isLoadingNextPage {
-            print(indexPath.row)
-            
-            //tableView.scrollToRow(at: indexPath.row - 1, at: .top, animated: true)
-        }
-        
         return cell
     }
     
     func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
-            //Check if the last cell is displayed
-            if indexPath.row == movies.count - 1 && !isLoadingNextPage {
-               print("this is the last page")
-                loadNextPage()
-            }
+        if indexPath.row == movies.count - 1 && !isLoadingNextPage {
+            print("this is the last cell of the current page")
+            loadNextPage()
         }
+    }
     
 }
 
-
-
-//func loadNextPage() {
-//    guard !isLoadingNextPage else {
-//        return
-//    }
-//
-//    isLoadingNextPage = true
-//    currentPage += 1
-//
-//    startBottomSpinner()
-//
-//    // Fetch movies on a background queue
-//    DispatchQueue.global().async {
-//        self.movieService.fetchMovies(page: self.currentPage) { newMovies in
-//            if let movies = newMovies {
-//                self.movies += movies
-//                DispatchQueue.main.async {
-//                    self.tableView.reloadData()
-//                    self.stopBottomSpinner()
-//                    self.isLoadingNextPage = false
-//                }
-//            } else {
-//                print("Failed to fetch more movies. No more pages.")
-//                // Handle error or show an alert
-//                DispatchQueue.main.async {
-//                    self.stopBottomSpinner()
-//                    self.isLoadingNextPage = false
-//                }
-//            }
-//        }
-//    }
-//}
-
-
-
-//guard !isLoadingNextPage else {
-//    return
-//}
-//
-//isLoadingNextPage = true
-//currentPage += 1
-//
-//
-//startBottomSpinner()
-//
-//DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
-//    self.loadMovies()
-//}
-//
-//stopBottomSpinner()
-//
-//isLoadingNextPage = false
